281901d603881d4b4b5f9b13d4154d737b312554
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-07-16 23:24:09 +0300

    headers for request added

---
+++ src/helpers/headers.service.ts
@@ -0,0 +1,24 @@
+export const getHeaders = {
+  accept: '*/*',
+  'accept-language': 'en-US,en;q=0.9',
+  'sec-ch-prefers-color-scheme': 'dark',
+  'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+  'sec-ch-ua-full-version-list':
+    '"Not.A/Brand";v="8.0.0.0", "Chromium";v="114.0.5735.198", "Google Chrome";v="114.0.5735.198"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"macOS"',
+  'sec-ch-ua-platform-version': '"13.4.1"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-origin',
+  'viewport-width': '730',
+  'x-asbd-id': '129477',
+  'x-csrftoken': 'W61Oof2Zad8a9FoiP0AQAafe9eU3N21x',
+  'x-ig-app-id': '936619743392459',
+  'x-ig-www-claim': 'hmac.AR2zUYz0rYW6os9Rtzk01H_vJ3RvvmQLCySJx558ZLe5aNWP',
+  'x-requested-with': 'XMLHttpRequest',
+  cookie:
+    'mid=Y5ZI2QAEAAH4SC_6fRNydmN_pF1l; ig_nrcb=1; ig_did=7C4B0CE8-6630-416C-892E-5C1760B0B32D; datr=2EiWY2REn8xtJBQOwMOZT2pD; csrftoken=W61Oof2Zad8a9FoiP0AQAafe9eU3N21x; ds_user_id=57057025143; dpr=2; shbid="12821\\05457057025143\\0541720988903:01f73b7045dc9a6f9237a838cafae98024a005a8b172169b64c989919f5802c0c12f5bee"; shbts="1689452903\\05457057025143\\0541720988903:01f794a800758d7f53a95c9a1347da17f3615f36dc959a0a871beb9a82078e5160d553fb"; sessionid=57057025143%3AOx2wph8dKrAXT3%3A20%3AAYdIgwZ3dV7sJ4US0MpWZTjIDi68Ypg2cH14_MOExQ; rur="CLN\\05457057025143\\0541720988914:01f7c7e044440d2f331b30854f2e1f1aed0ba9d0c2d052b35416fef4848f1d571e51ad2c"',
+  Referer: 'https://www.instagram.com/bmw/',
+  'Referrer-Policy': 'strict-origin-when-cross-origin',
+};
