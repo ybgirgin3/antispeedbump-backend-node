5858355bbc65475c5478e3f7919364093e485841
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-07-20 22:06:34 +0300

    scraper response type converted to `ScrapeResponseDto`

--- src/helpers/scrape.parser.ts
+++ src/helpers/scrape.parser.ts
@@ -1,27 +1,20 @@
-export const scrapeParser = (data: object) => {
-  const ret = {};
+import { ScrapeResponseDto } from './db/dto/antispeed.dto';
+
+export const scrapeParser = (data: object): ScrapeResponseDto => {
   const user_data = data['data']['user'];
   const medias = user_data['edge_owner_to_timeline_media'];
 
-  // text
-  ret['id'] = user_data['id'];
-  ret['username'] = user_data['username'];
-  ret['profile_picture'] = user_data['profile_pic_url_hd'];
-  ret['full_name'] = user_data['full_name'];
-  ret['biography'] = user_data['biography'];
-  ret['bio_links'] = user_data['bio_links'];
-  ret['is_private'] = user_data['is_private'];
-  ret['pronouns'] = user_data['pronouns'];
-
-  if (user_data['is_private']) {
-    console.log(
-      'Account is private, Data will be limited. And no media will be included',
-    );
-    return ret;
-  }
-
-  // posts / media
-  ret['medias'] = { count: medias['count'], media: medias['edges'] };
-
-  return ret;
+  return {
+    id: user_data['id'],
+    username: user_data['username'],
+    profile_picture: user_data['profile_pic_url_hd'],
+    full_name: user_data['full_name'],
+    biography: user_data['biography'],
+    bio_links: user_data['bio_links'],
+    pronouns: user_data['pronouns'],
+    is_private: user_data['is_private'],
+    medias: user_data['is_private']
+      ? {}
+      : { count: medias['count'], media: medias['edges'] },
+  };
 };
