db50848d9719ddabe15c23d3d627ec9f5d099ccb
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-07-20 22:07:04 +0300

    Mongoose connection done

--- src/scraper/scraper.module.ts
+++ src/scraper/scraper.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ScraperService } from './scraper.service';
 import { ScraperController } from './scraper.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  AntiSpeed,
+  AntiSpeedSchema,
+} from '../helpers/db/schemas/antispeed.schema';
 
 @Module({
   controllers: [ScraperController],
   providers: [ScraperService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: AntiSpeed.name, schema: AntiSpeedSchema },
+    ]),
+  ],
 })
 export class ScraperModule {}
