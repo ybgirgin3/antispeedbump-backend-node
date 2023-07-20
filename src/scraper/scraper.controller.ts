bdb2a3e3e7ad23c9c6d77d6d8ee2eefb5b49c41a
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-07-20 22:06:53 +0300

    Dto controlling

--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -1,21 +1,27 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ScraperService } from './scraper.service';
+import { DbService } from '../helpers/db/db.service';
 import { ScrapeDto } from './dto/scrape.dto';
 import { scrapeParser } from 'src/helpers/scrape.parser';
 
+import { ScrapeResponseDto } from '../helpers/db/dto/antispeed.dto';
+
 @Controller('scraper')
 export class ScraperController {
-  constructor(private readonly scraperService: ScraperService) {}
+  constructor(
+    private readonly scraperService: ScraperService, // private readonly dbService: DbService,
+  ) {}
 
-  @Post('scrape')
+  @Post('getFromAnother')
   async scrape(@Body() scrapeDto: ScrapeDto) {
     const rawData = await this.scraperService.fetch(scrapeDto);
     if (rawData.errorResponse === null) {
+      const resp: ScrapeResponseDto = scrapeParser(rawData.scrapeResponse);
       return {
         status: 200,
         message: 'Data fetch and parsed successfully',
         error: rawData.errorResponse,
-        response: scrapeParser(rawData.scrapeResponse),
+        response: resp,
       };
     } else {
       return {
