901b56bf0fce97991918ce7bc0df160790c3c4fa
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-07-20 22:07:32 +0300

    make app connectable with mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,21 @@ import { AppService } from './app.service';
 import { ScraperService } from './scraper/scraper.service';
 import { ScraperController } from './scraper/scraper.controller';
 import { ScraperModule } from './scraper/scraper.module';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [AppController, ScraperController],
   providers: [AppService, ScraperService],
-  imports: [ScraperModule],
+  imports: [
+    ScraperModule,
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.getMongoConfig(),
+    }),
+  ],
 })
 export class AppModule {}
